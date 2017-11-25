08762237be017b9c8c1ccad368dd7fee5a5474bc
lixiaolong <hy1404381881@163.com>
2017-11-25 14:12:09 +0800

    注解和数据验证

---
+++ DataValidation/src/main/java/org/xdemo/validation/RegexType.java
@@ -0,0 +1,18 @@
+package org.xdemo.validation;
+
+/**
+ * 常用的数据类型枚举
+ * @author Goofy
+ *
+ */
+public enum RegexType {
+	
+	NONE,
+	SPECIALCHAR,
+	CHINESE,
+	EMAIL,
+	IP, 
+	NUMBER,
+	PHONENUMBER;
+	
+}
---
+++ DataValidation/src/main/java/org/xdemo/validation/test/Test.java
@@ -0,0 +1,29 @@
+package org.xdemo.validation.test;
+
+import org.xdemo.validation.annotation.support.ValidateService;
+
+/**
+ * @author Goofy
+ */
+public class Test {
+	public static void main(String[] args){
+		User user=new User("张三", "xdemo.org", "252878950@qq.com");
+		try {
+			ValidateService.valid(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		user=new User("zhangsan","xdemo.org","xxx@");
+		try {
+			ValidateService.valid(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		user=new User("zhangsan","xdemo.org","");
+		try {
+			ValidateService.valid(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
